8538e7341a37553c27b38086602d7e1c75d7152e
zileyuan <zileyuan@gmail.com>
2017-06-10 13:51:23 +0800

    remove some unused files

---
+++ src/app/scene/login.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+import { Text } from 'react-native';
+
+export default class Login extends React.Component<object, object>{
+    render() {
+        return (
+            <Text>Hello world!</Text>
+        );
+    }
+}
